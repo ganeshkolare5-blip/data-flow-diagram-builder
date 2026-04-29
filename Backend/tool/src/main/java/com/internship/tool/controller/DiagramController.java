430ed46303a1f12902e1bec3c5b66c7b3b0e88c0
ganeshkolare5-blip <ganeshkolare5@gmail.com>
2026-04-29 16:14:04 +0530

    Day 2: Work is completed

--- Backend/tool/src/main/java/com/internship/tool/controller/DiagramController.java
+++ Backend/tool/src/main/java/com/internship/tool/controller/DiagramController.java
@@ -3,7 +3,11 @@ package com.internship.tool.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.internship.tool.entity.Diagram;
 import com.internship.tool.service.DiagramService;
@@ -15,7 +19,7 @@ public class DiagramController {
     @Autowired
     private DiagramService service;
 
-    // ✅ ROOT CHECK API
+    // ✅ ROOT home CHECK API 
     @GetMapping("/")
     public String home() {
         return "Backend Connected Successfully ";
